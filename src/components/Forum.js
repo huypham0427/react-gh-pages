9715f2e19b968c24a61f15464b12720e843c9dc9
huypham0427 <ptanhhuy27@yahoo.com>
2023-08-28 12:04:02 -0700

    testing-fix url + complete Live Chat

--- src/components/Forum.js
+++ src/components/Forum.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { ChatEngine} from 'react-chat-engine';
 import ChatFeed from '../components/ChatFeed.js';
+import '../styling/ChatEngine.css';
+import LoginForm from '../components/LoginForm.js';
 
 function Forum() {
+
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
     <ChatEngine 
         height="100vh"
         projectID="8c9327c6-e26b-43ec-88d1-cb38451fb78d"
-        userName="admin"
-        userSecret="admin"
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
         renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   )
